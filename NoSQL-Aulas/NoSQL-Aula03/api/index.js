bd13030a00c845c4a8b6ff22512155905538333d
Gabriel <59896608+ashkgabriel@users.noreply.github.com>
2024-03-05 19:20:18 -0300

    Add files via upload

---
+++ NoSQL-Aulas/NoSQL-Aula03/api/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+
+const app = express()
+const port = 4000
+
+// Efetuaremos o parse do JSON
+
+app.use(express.json())
+
+// Rotas de conteúdo público
+
+app.use('/', express.static('public'))
+
+// Primeira rota pública
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'API ok!', version: '1.0' })
+})
+
+// Tratar rotas inválidas
+
+app.use(function (req, res) {
+    res.status(404).json({
+        errors: [{
+            value: `${req.originalUrl}`,
+            msg: `A rota ${req.originalUrl} não existee.`,
+            param: 'Invalid route.'
+        }]
+    })
+})
+
+// Ouvider - listen
+
+app.listen(port, function () {
+    console.log(`💻 Servidor rodando em ${port}`);
+})
